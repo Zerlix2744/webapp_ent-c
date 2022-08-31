498c9445232433bc18b156a512e5e171d5786d30
jay <monk27443@gmail.com>
2022-08-31 10:14:57 +0700

    done verify

---
+++ verify.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <form action="verify.php" method="post"></form>
+    <h1 align="center">Zerlix Webboard</h1><hr>
+    <br>
+    <div align="center">
+        เข้าสู่ระบบด้วย<br>
+        Login = <?php echo $_POST["login"];?><br>
+        Password = <?php echo $_POST["password"];?>
+    </div>
+    </form>
+</body>
+</html>
