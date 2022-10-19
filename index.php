af9e4d8869277fc49d7b227bb1a916869617cc21
Zerlix2744 <110074212+Zerlix2744@users.noreply.github.com>
2022-10-19 12:08:56 +0700

    add nav.php-edit index.php

--- index.php
+++ index.php
@@ -7,35 +7,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <title>Zerlix Webboard</title>
 </head>
 <?php
     if(!isset($_SESSION['id'])){
 ?>
 <body>
-    <form action="post.php" method="post">
-    <h1 style="text-align:center">Zerlix Webboard</h1><hr>
-    หมวดหมู่ : <select name="หมวดหมู่">
-        <option value="--ทั้งหมด--">--ทั้งหมด--</option>
-        <option value="เรื่องทั่วไป">เรื่องทั่วไป</option>
-        <option value="เรื่องเรียน">เรื่องเรียน</option>
-    </select>
-    <a href="login.php" style="float: right">เข้าสู่ระบบ</a>
-    <ul>
-        <?php
-            for($i=1;$i<=10;$i++){
-                echo "<li><a href=post.php?id=$i>กระทู้ที่$i</a></li>";
-            }
-        ?>
-    </ul>
-    </form>
+    <div class="container">
+        <h1 style="text-align:center">Zerlix Webboard</h1>
+        <?php include "nav.php"; ?>
+        <br>
+        <div class="d-flex">
+            <div>
+                <label>หมวดหมู่</label>
+                <span class="dropdown">
+                    <button class="btn btn-light dropdown-toggle btn-sm"
+                        type="button" id="button2" data-bs-toggle="dropdown"
+                        aria-expanded="false">--ทั้งหมด--
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="button2">
+                        <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                        <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+                        <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                    </ul>
+                </span>
+            </div>
+        </div>
+        <br>
+        <table class="table table-striped">
+            <?php 
+                for($i=1;$i<=10;$i++){
+                echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้ที่ $i</td></tr>";
+                }
+            ?>
+         </table>   
+    </div>
 </body>
 <?php
     }else{
 ?>
 <body>
+<div class="container">
+    <h1 style="text-align:center">Zerlix Webboard</h1>
+    <?php include "nav.php"; ?><hr>
     <form action="post.php" method="post">
-    <h1 style="text-align:center">Zerlix Webboard</h1><hr>
     หมวดหมู่ : <select name="หมวดหมู่">
         <option value="--ทั้งหมด--">--ทั้งหมด--</option>
         <option value="เรื่องทั่วไป">เรื่องทั่วไป</option>
@@ -61,6 +79,7 @@
         ?>
     </ul>
     </form>
+</div>    
 </body>
 <?php
     }
